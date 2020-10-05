516d363e53a91b826e8b4e0019035881132dd3fb
Liam Cristello <liamcristello@gmail.com>
2020-10-05 19:51:09 -0400

    Some basic set-up, car testing
    Car can move forward, don't know if I love ramp up

---
+++ Road Trip/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Position of camera from object
+    Vector3 offset;
+    // The object for the camera to follow
+    [Tooltip("The object for the camera to follow.")]
+    [SerializeField] Transform followedObject;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = followedObject.position + offset;
+    }
+}
---
+++ Road Trip/Assets/Scripts/MoveCar.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class MoveCar : MonoBehaviour
+{
+    // Base car values
+    [Tooltip("How much force the car engine has.")] 
+    [SerializeField] double enginePower = 15.0;
+
+    [Tooltip("The maximum amount the car can be steered in one direction.")] 
+    [SerializeField] double maxSteer = 10.0;
+
+    // Calculating how fast car is moving
+    [Tooltip("Current speed the car is traveling at.")]
+    [SerializeField] double speed = 0.0;
+    //Vector3 previousPosition = ;
+
+    // Input from player
+    double gas;
+    double brake;
+    double steer;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //vel = 
+        gas = Input.GetAxis("Vertical") * enginePower * Time.deltaTime;
+        steer = Input.GetAxis("Horizontal") * enginePower * Time.deltaTime;
+        rb.AddForce(transform.forward * (float) gas, ForceMode.Impulse);
+        //rb.MoveRotation(steer);
+    }
+}
