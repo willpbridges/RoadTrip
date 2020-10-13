ef7b607fe35c5f54f4d733f597504f6045a703f7
Liam Cristello <liamcristello@gmail.com>
2020-10-12 21:39:52 -0400

    Car moves around and is good
    Yay :smiley:

--- Road Trip/Assets/Scripts/CameraFollow.cs
+++ Road Trip/Assets/Scripts/CameraFollow.cs
@@ -1,25 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Position of camera from object
-    Vector3 offset;
-    // The object for the camera to follow
-    [Tooltip("The object for the camera to follow.")]
-    [SerializeField] Transform followedObject;
+    //youtube.com/watch?v=Z4HA8zJhGEks
 
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private Transform target;
+    [SerializeField] private float translateSpeed;
+    [SerializeField] private float rotationSpeed;
 
-    // Start is called before the first frame update
-    void Start()
+    private void FixedUpdate()
     {
-        offset = transform.position;
+        HandleTranslation();
+        HandleRotation();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void HandleTranslation()
     {
-        transform.position = followedObject.position + offset;
+        var targetPosition = target.TransformPoint(offset);
+        // Bad? Use SmoothDamp method instead? Idk that's what the guy said
+        transform.position = Vector3.Lerp(transform.position, targetPosition, translateSpeed * Time.deltaTime);
+    }
+
+    private void HandleRotation()
+    {
+        var direction = target.position - transform.position;
+        // See above comment also about Lerp ?
+        var rotation = Quaternion.LookRotation(direction, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
     }
 }
